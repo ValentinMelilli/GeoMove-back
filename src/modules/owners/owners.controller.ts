638d240afd1187d47d2f9e0101b888e0f62b72e0
ValentinMelilli <melilli.valentin@gmail.com>
2023-05-24 14:54:52 +0200

    Updated owner comments

--- src/modules/owners/owners.controller.ts
+++ src/modules/owners/owners.controller.ts
@@ -11,53 +11,53 @@ export class OwnersController {
 
     @Get()
     async findAll() {
-        // get all structures in the db
+        // get all owners in the db
         return await this.ownerService.findAll();
     }
 
     @Get(':id')
     async findOne(@Param('id') id: number): Promise<OwnerEntity> {
-        // find the structure with this id
+        // find the owner with this id
         const owner = await this.ownerService.findOne(id);
 
-        // if the structure doesn't exit in the db, throw a 404 error
+        // if the owner doesn't exit in the db, throw a 404 error
         if (!owner) {
             throw new NotFoundException('This Owner doesn\'t exist');
         }
 
-        // if structure exist, return the structure
+        // if owner exist, return the owner
         return owner;
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Post()
     async create(@Body() owner: OwnerDto, @Request() req): Promise<OwnerEntity> {
-        // create a new structure and return the newly created structure
+        // create a new owner and return the newly created owner
         return await this.ownerService.create(owner, req.user.id);
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Put(':id')
     async update(@Param('id') id: number, @Body() owner: OwnerDto, @Request() req): Promise<OwnerEntity> {
-        // get the number of row affected and the updated structure
+        // get the number of row affected and the updated owner
         const { numberOfAffectedRows, updatedOwner } = await this.ownerService.update(id, owner, req.user.id);
 
-        // if the number of row affected is zero, it means the structure doesn't exist in our db
+        // if the number of row affected is zero, it means the owner doesn't exist in our db
         if (numberOfAffectedRows === 0) {
             throw new NotFoundException('This Owner doesn\'t exist');
         }
 
-        // return the updated structure
+        // return the updated owner
         return updatedOwner;
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Delete(':id')
     async remove(@Param('id') id: number, @Request() req) {
-        // delete the structure with this id
+        // delete the owner with this id
         const deleted = await this.ownerService.delete(id, req.user.id);
 
-        // if the number of row affected is zero, then the structure doesn't exist in our db
+        // if the number of row affected is zero, then the owner doesn't exist in our db
         if (deleted === 0) {
             throw new NotFoundException('This Owner doesn\'t exist');
         }
