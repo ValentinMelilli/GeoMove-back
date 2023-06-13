b9d06869b6063529069b7a4cbb873023e48c90c2
ValentinMelilli <melilli.valentin@gmail.com>
2023-06-13 12:58:15 +0200

    Removed Sport tags controller

--- src/modules/sports_tags/sports_tags.module.ts
+++ src/modules/sports_tags/sports_tags.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { SportsTagsService } from './sports_tags.service';
 import { sportsTagsProviders } from './sports_tags.providers';
-import { SportsTagsController } from './sports_tags.controller';
 
 @Module({
   providers: [SportsTagsService, ...sportsTagsProviders],
-  controllers: [SportsTagsController],
+  exports: [SportsTagsService],
 })
 export class SportsTagsModule {}
